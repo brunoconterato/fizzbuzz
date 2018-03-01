a4b34d755f61184aafeced56f39b985559698b3c
Bruno Conterato <brunoconterato@gmail.com>
2018-03-01 14:43:11 -0300

    Adicionados novos testes

--- FizzBuzz.Tests/Tests.fs
+++ FizzBuzz.Tests/Tests.fs
@@ -9,8 +9,19 @@ let ``My test`` () =
     Assert.True(true)
 
 [<Fact>]
-let ``fizzBuzz function test`` () =
+let ``fizzBuzz function test 1`` () =
     let expected = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz"
-    // let actual = (main [|"5"|])
     let actual = (fizzBuzz 20)
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``fizzBuzz function test 2`` () =
+    let expected = "1\n2\nFizz\n4\nBuzz"
+    let actual = (fizzBuzz 5)
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``fizzBuzz function test 3`` () =
+    let expected = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11"
+    let actual = (fizzBuzz 11)
     Assert.Equal(expected, actual)
